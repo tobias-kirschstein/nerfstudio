22fd34c1786c1dbe7941a3ce2fb34e013575877d
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2022-11-02 18:58:38 +0100

    Fix bg wandb eval

--- nerfstudio/models/instant_ngp.py
+++ nerfstudio/models/instant_ngp.py
@@ -523,7 +523,7 @@ class NGPModel(Model):
                 # TODO: subtract -0.5 from background_pixels to make the effort for the bg network symmetric?
                 # background_pixels = torch.sigmoid(background_pixels - 0.5 + 10 * outputs["background_adjustments"] - 5)
 
-                ba = outputs["background_adjustments"].mean(dim=1)
+                ba = outputs["background_adjustments"].mean(dim=-1)
                 alpha = (4 * ba.pow(2) - 4 * ba + 1)  # alpha(ba=0|1) -> 1, alpha(ba=0.5) -> 0
                 alpha = alpha.unsqueeze(-1)  # [R, 1]
                 background_pixels = ((1 - alpha) * background_pixels + alpha * outputs["background_adjustments"])
