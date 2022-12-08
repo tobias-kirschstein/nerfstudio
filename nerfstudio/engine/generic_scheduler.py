7d122fd127988e272f0104d786d92bff1479e349
Shenhan Qian <shenhan.qian@tum.de>
2022-12-09 00:48:07 +0100

    Fix a bug up GenericScheduler

--- nerfstudio/engine/generic_scheduler.py
+++ nerfstudio/engine/generic_scheduler.py
@@ -19,7 +19,7 @@ class GenericScheduler(torch.nn.Module):
         elif step < self.begin_step:
             self.value = self.init_value
         else:
-            self.value = min(max((step - self.begin_step) / self.end_step, 0), 1) * self.final_value
+            self.value = min(max((step - self.begin_step) / (self.end_step - self.begin_step), 0), 1) * self.final_value
 
     def get_value(self):
         if self.training:  # inherit from torch.nn.Module
