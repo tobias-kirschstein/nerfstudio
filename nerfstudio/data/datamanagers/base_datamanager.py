Add data worker seeding + allow optimizers without gradients + fix instant ngp background color mismatch

nerfstudio/data/datamanagers/base_datamanager.py
@@ -227,7 +227,7 @@ class DataManager(nn.Module):
         raise NotImplementedError
 
     def get_training_callbacks(  # pylint:disable=no-self-use
-        self, training_callback_attributes: TrainingCallbackAttributes  # pylint: disable=unused-argument
+            self, training_callback_attributes: TrainingCallbackAttributes  # pylint: disable=unused-argument
     ) -> List[TrainingCallback]:
         """Returns a list of callbacks to be used during training."""
         return []
@@ -290,6 +290,7 @@ class VanillaDataManagerConfig(InstantiateConfig):
 
     n_steps_warmup: int = -1  # If set, during warmup only the first timestep will be sampled
     n_timesteps_warmup: int = -1  # How many keyframes will be used during warmup
+    seed: Optional[int] = None  # Seed for dataloader workers
 
 
 class VanillaDataManager(DataManager):  # pylint: disable=abstract-method
@@ -310,13 +311,13 @@ class VanillaDataManager(DataManager):  # pylint: disable=abstract-method
     eval_dataset: InputDataset
 
     def __init__(
-        self,
-        config: VanillaDataManagerConfig,
-        device: Union[torch.device, str] = "cpu",
-        test_mode: Literal["test", "val", "inference"] = "val",
-        world_size: int = 1,
-        local_rank: int = 0,
-        **kwargs,  # pylint: disable=unused-argument
+            self,
+            config: VanillaDataManagerConfig,
+            device: Union[torch.device, str] = "cpu",
+            test_mode: Literal["test", "val", "inference"] = "val",
+            world_size: int = 1,
+            local_rank: int = 0,
+            **kwargs,  # pylint: disable=unused-argument
     ):
         self.config = config
         self.device = device
@@ -357,6 +358,7 @@ class VanillaDataManager(DataManager):  # pylint: disable=abstract-method
             num_workers=self.world_size * 4,
             pin_memory=True,
             collate_fn=self.config.collate_fn,
+            seed=self.config.seed
         )
         self.iter_train_image_dataloader = iter(self.train_image_dataloader)
         self.train_pixel_sampler = PixelSampler(self.config.train_num_rays_per_batch,
@@ -374,6 +376,7 @@ class VanillaDataManager(DataManager):  # pylint: disable=abstract-method
             image_indices=self.config.train_image_indices,
             device=self.device,
             num_workers=self.world_size * 4,
+            seed=self.config.seed
         )
 
     def setup_eval(self):
@@ -388,6 +391,7 @@ class VanillaDataManager(DataManager):  # pylint: disable=abstract-method
             num_workers=self.world_size * 4,
             pin_memory=True,
             collate_fn=self.config.collate_fn,
+            seed=self.config.seed
         )
         self.iter_eval_image_dataloader = iter(self.eval_image_dataloader)
         self.eval_pixel_sampler = PixelSampler(self.config.eval_num_rays_per_batch,
@@ -401,12 +405,14 @@ class VanillaDataManager(DataManager):  # pylint: disable=abstract-method
             input_dataset=self.eval_dataset,
             device=self.device,
             num_workers=self.world_size * 4,
+            seed=self.config.seed
         )
         self.eval_dataloader = RandIndicesEvalDataloader(
             input_dataset=self.eval_dataset,
             image_indices=self.config.eval_image_indices,
             device=self.device,
             num_workers=self.world_size * 4,
+            seed=self.config.seed
         )
 
     def next_train(self, step: int) -> Tuple[RayBundle, Dict]:

nerfstudio/data/utils/dataloaders.py
@@ -23,6 +23,7 @@ import random
 from abc import abstractmethod
 from typing import Dict, Optional, Tuple, Union, List
 
+import numpy as np
 import torch
 from rich.progress import Console, track
 from torch.utils.data import Dataset
@@ -35,6 +36,12 @@ from nerfstudio.data.utils.nerfstudio_collate import nerfstudio_collate
 from nerfstudio.utils.misc import get_dict_to_torch
 
 CONSOLE = Console(width=120)
+
+
+def _seed_worker(seed: int):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
 
 
 class CacheDataloader(DataLoader):
@@ -50,16 +57,19 @@ class CacheDataloader(DataLoader):
     """
 
     def __init__(
-        self,
-        dataset: Dataset,
-        num_images_to_sample_from: int = -1,
-        num_times_to_repeat_images: int = -1,
-        device: Union[torch.device, str] = "cpu",
-        collate_fn=nerfstudio_collate,
-        **kwargs,
+            self,
+            dataset: Dataset,
+            num_images_to_sample_from: int = -1,
+            num_times_to_repeat_images: int = -1,
+            device: Union[torch.device, str] = "cpu",
+            collate_fn=nerfstudio_collate,
+            seed: Optional[int] = None,
+            **kwargs,
     ):
         self.dataset = dataset
-        super().__init__(dataset=dataset, **kwargs)  # This will set self.dataset
+        super().__init__(dataset=dataset,
+                         worker_init_fn=None if seed is None else lambda worker_id: _seed_worker(seed + worker_id),
+                         **kwargs)  # This will set self.dataset
         self.num_times_to_repeat_images = num_times_to_repeat_images
         self.cache_all_images = (num_images_to_sample_from == -1) or (num_images_to_sample_from >= len(self.dataset))
         self.num_images_to_sample_from = len(self.dataset) if self.cache_all_images else num_images_to_sample_from
@@ -171,13 +181,15 @@ class EvalDataloader(DataLoader):
             self,
             input_dataset: InputDataset,
             device: Union[torch.device, str] = "cpu",
+            seed: Optional[int] = None,
             **kwargs,
     ):
         self.input_dataset = input_dataset
         self.cameras = input_dataset.cameras.to(device)
         self.device = device
         self.kwargs = kwargs
-        super().__init__(dataset=input_dataset)
+        super().__init__(dataset=input_dataset,
+                         worker_init_fn=None if seed is None else lambda worker_id: _seed_worker(seed + worker_id))
 
     @abstractmethod
     def __iter__(self):
@@ -228,9 +240,10 @@ class FixedIndicesEvalDataloader(EvalDataloader):
             input_dataset: InputDataset,
             image_indices: Optional[Tuple[int]] = None,
             device: Union[torch.device, str] = "cpu",
+            seed: Optional[int] = None,
             **kwargs,
     ):
-        super().__init__(input_dataset, device, **kwargs)
+        super().__init__(input_dataset, device, seed=seed, **kwargs)
         if image_indices is None:
             self.image_indices = list(range(len(input_dataset)))
         else:
@@ -262,9 +275,10 @@ class RandIndicesEvalDataloader(EvalDataloader):
             self,
             input_dataset: InputDataset,
             device: Union[torch.device, str] = "cpu",
+            seed: Optional[int] = None,
             **kwargs,
     ):
-        super().__init__(input_dataset, device, **kwargs)
+        super().__init__(input_dataset, device, seed=seed, **kwargs)
         self.count = 0
 
     def __iter__(self):